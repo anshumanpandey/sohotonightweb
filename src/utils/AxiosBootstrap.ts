473a435901fecbce026b1d012e38c11e28cf60e7
leo <leo_9_t@hotmail.com>
2020-10-16 16:29:18 -0400

    ADDED debug

--- src/utils/AxiosBootstrap.ts
+++ src/utils/AxiosBootstrap.ts
@@ -6,6 +6,7 @@ const AxiosInstance = Axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 })
 AxiosInstance.interceptors.request.use((r) => {
+  console.log(getGlobalState())
   const token = getGlobalState().jtwToken
   if (token) {
     r.headers.Authorization = `Bearer ${token}`

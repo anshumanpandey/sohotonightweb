8b5cb050eac38b800857684f7459e01505778602
leo <leo_9_t@hotmail.com>
2020-11-05 12:58:54 -0400

    ADDED missing page

---
+++ src/pages/marketplaceAgreement/MarketplaceAgreement.page.tsx
@@ -0,0 +1,188 @@
+import React from 'react';
+import Footer from '../../partials/Footer';
+import NavBar from '../../partials/NavBar';
+
+function MarketplaceAgreement() {
+    return (
+        <>
+            <NavBar />
+            <div className="container page-content ">
+                <div className="row">
+                    <div className="col-md-12 col-sm-12 col-xs-12">
+
+                        <div className="row">
+                            <div className="col-md-12 col-sm-12 col-xs-12">
+                                <div className="widget">
+
+                                    <div className="widget-body">
+
+
+                                        <div className="main-contact-section">
+
+                                            <h2>Statements & Opinion </h2>
+
+                                            <h3>Transparency </h3>
+                                            <p>With its registered office at Sohotonight Limited, Lytchett House, 13 Freeland Park, Wareham Road, Poole, Dorset, BH16 6FA the Principal registered office. </p>
+
+                                            <h3>Websites</h3>
+                                            <p>We operate the following websites: </p>
+                                            <ul>
+                                                <li>www.sohotonight.com</li>
+                                                <li>www.camsonly.co.uk</li>
+                                            </ul>
+                                            <p>All parties and websites mentioned above are hereafter referred to as "ST". </p>
+
+
+                                            <h3>Purpose of this Agreement </h3>
+
+                                            <p>
+                                                ST has a Marketplace that allows Users to offer, sell and buy various goods. ST also allows members to purchase items from the Official Stores which are provided by third-party suppliers. 
+                                            </p>
+                                            <p>
+                                            The provisions of this Agreement outline the acceptable and prohibited use of these areas of the ST site(s) as well as informing you of delivery/returns information for the Official Stores. 
+                                            </p>
+
+                                            <p style={{ fontWeight: 'bold' }}>Definitions </p>
+                                            <p>
+                                                Within this Agreement, Buyers may also be Sellers and Sellers may also be Buyers. Therefore, both Buyers and Sellers are collectively referred to as Users and/or Members, unless specific terms only apply to either Buyers or Sellers, which are referenced accordingly herein. 
+                                            </p>
+                                            <p>
+                                                The Marketplace is the area of the site where User-to-User Sales/Auctions can be conducted. Official Stores orders are fulfilled by our suppliers. The Marketplace and Official Stores are collectively referred to as the “Mall”. 
+                                            </p>
+                                            <p>
+                                                A “Sale” is where an item is listed for a designated price by the Seller and purchased by the Buyer. A “Random Winner Sale” is similar to a raffle where Buyers essentially are purchasing a chance at winning the item, more information on that here. 
+                                            </p>
+                                            <p>
+                                                An “Auction” functions in the same way as you would expect; Buyers place bids to win the Auction and receive the item. Sellers can select a minimum bid value and reserve price for each Auction. 
+                                            </p>
+
+                                            <h3>Terms and Conditions of the Marketplace </h3>
+                                            <p>
+                                                If you participate in an Auction or Sale, you are acknowledging that you have read, understood and accept this Marketplace Agreement. 
+                                            </p>
+                                            <p>
+                                                This Agreement should be read in the context of the Site’s Policies & Agreements as a whole. If there are any inconsistencies between them, the ST Site(s) User Agreement, which expresses your rights and obligations more completely, will take precedence over this Marketplace Agreement. 
+                                            </p>
+                                            <p>
+                                                Bids, Purchases and Payment 
+                                            </p>
+
+
+                                            <p style={{ fontWeight: 'bold' }}>Bids and Purchases </p>
+                                            <p>
+                                                Unless otherwise specified in this Agreement, bids placed on all Auctions and all purchases made will be binding. This means that if a bid is placed in an Auction or purchase made in a Sale through your ST account, whether by you or someone who uses that account, the bid/purchase may not be withdrawn and is binding. If your bid is successful, a contract is automatically created between you and the Seller when the Auction closes. With a Sale, the contract is made at the point of committing the purchase. 
+                                            </p>
+                                            <p>
+                                                It is the receipt by the ST system of the relevant bid/purchase which will constitute the bid/purchase, not the dispatch of the bid/purchase from the Member's computer or other device. ST will use its reasonable endeavors to process bids and purchases which are placed but does not guarantee that each individual bid/purchase will necessarily be submitted. 
+                                            </p>
+
+                                            <p style={{ fontWeight: 'bold' }}>Payment</p>
+                                            <p>
+                                                When a Sale is made or an Auction ends, the relevant number of credits will be debited from the Buyer. These credits will be held in escrow, where neither party can access them, for 7 days after the item has been marked as dispatched. If the Buyer has not advised us of non-delivery within those 7 days after dispatch, the credits will be transferred to the Seller. 
+                                            </p>
+                                            <p>
+                                                All Auction items and Random Winner format sale items require the winner to enter their delivery details within a period of 7 days from the date the Auction ends or the Random Winner is selected/published. If the Buyer does not enter the delivery details and the Seller is unable to contact them, the credits will remain in escrow for 28 days, after which time the Seller will become entitled to them. 
+                                            </p>
+
+                                            <p style={{ fontWeight: 'bold' }}>Disputes Between Users </p>
+                                            <p>
+                                                In the event of a dispute, it will be up to the Buyer and Seller to resolve the dispute and advise of the required action with regard to the transfer of credits. 
+                                            </p>
+                                            <p>
+                                                ST does not intervene unless either Member is able to provide supporting evidence to verify their claim or in other exceptional circumstances. 
+                                            </p>
+
+                                            <p style={{ fontWeight: 'bold' }}>Returns</p>
+                                            <p>
+                                                In all circumstances Sellers and Buyers are required to agree the disputes and any return shipping costs between themselves and notify ST is the instance where some action is required from ST. 
+                                            </p>
+
+                                            <p style={{ fontWeight: 'bold' }}>Abusing ST </p>
+                                            <p>
+                                                You must not bid in a way that is manipulative of an auction. In particular, if you are the Seller, you may not bid in your own Auction, whether through your own ST account, or through another Member's account. 
+                                            </p>
+                                            <p>
+                                                Without limiting our access to additional remedies, we reserve the right to suspend or terminate your user account, limit your ability to access the Marketplace, cancel bids, remove or restrict visibility of your listing(s) and take any necessary technical or legal measures to restrict your ability to use the services of ST if it is reasonable for us to believe that: 
+                                            </p>
+                                            <ul>
+                                                <li>Your listings fall under the category of Prohibited Listings. </li>
+                                                <li>You are creating or are likely to create problems for the site and/or legal liabilities. </li>
+                                                <li>You are infringing third party rights. </li>
+                                                <li>Your account is not genuine and we cannot verify information you provide to us.  </li>
+                                            </ul>
+                                            <p>Listing Items for Sale </p>
+                                            <p>The placement of a listing, with items included for an Auction or Sale, through your ST account (whether by you or someone who uses that account) is binding and may not be withdrawn or changed once the first bid/purchase in that Auction/Sale is placed or made. </p>
+                                            <p>We reserve the right to withdraw a listing, even during the course of an Auction/Sale, which appears to be in breach of the Site’s Policies & Agreements. In that event, we will inform the Seller and potentially, where an Auction is being conducted based on that listing, one or more Buyers participating in that Auction of the withdrawal in question. </p>
+                                            
+                                            <p style={{ fontWeight: 'bold' }}>Prohibited Listings </p>
+                                            <p>The facilities within the Marketplace i.e., Auctions, Sales and Random Winner items are for tangible goods only. Tangible goods are those items that can be physically delivered to an address using the postal system. </p>
+                                            <p style={{ fontWeight: 'bold' }}>These items listed below cannot and must not be sold via the Marketplace or Member's profiles. </p>
+                                            <ul>
+                                                <li>Advice, spells, curses, hexing, conjuring, magic, prayers, blessing services, magic potions & healing sessions </li>
+                                                <li>Alcohol, Tobacco, Tobacco Products, Drugs, Drug paraphernalia, Herbal remedies & Herbal performance enhancers </li>
+                                                <li>Animals and Wildlife Products </li>
+                                                <li>Food/Drink Items  </li>
+                                                <li>Bodily Fluids (used underwear may be sold but not those that include bodily fluids) </li>
+                                                <li>Children's items (including clothing & toys) </li>
+                                                <li>Condoms (new or used)  </li>
+                                                <li>Counterfeit & Replica items (including clothing, jewelry, currency & stamps)  </li>
+                                                <li>Credit Cards  </li>
+                                                <li>Donations & Tributes </li>
+                                                <li>Escort related activities (bookings, deposits, random winner items) involving time or companionship meetings as a reward </li>
+                                                <li>Firearms, Ammunition, Weapons & Knives </li>
+                                                <li>Government IDs, Licenses and Uniforms  </li>
+                                                <li>Hazardous & Restricted Items  </li>
+                                                <li>Lottery Tickets </li>
+                                                <li>Mailing Lists and Personal Information</li>
+                                                <li>Monetary gifts, Vouchers and Gift Cards </li>
+                                                <li>Multi-level Marketing, Pyramid, Matrix and Trading Schemes  </li>
+                                                <li>Offensive Material </li>
+                                                <li>Pre-recorded or Custom-made Videos, DVDs, Blu-Ray, CDs (Adult or Non-Adult)  </li>
+                                                <li>Prescription only medicine, (including Viagra and Cialis or any generic variants of prescription drugs) </li>
+                                                <li>Satellite, Digital and Cable TV Decoders </li>
+                                                <li>Skype, Yahoo (etc.) and SMS-based chat sessions </li>
+                                                <li>Stolen Items</li>
+                                                <li>Surveillance Equipment </li>
+                                                <li>Travel tickets, Holidays, Sporting & Event Tickets  </li>
+                                                <li>Unauthorized Copies (audio and video content, including 'backup copies')  </li>
+                                                <li>Unlocking Software  </li>
+                                                <li>Webcam or Phone Chat sessions </li>
+                                                <li>Any other items for which is it illegal to sell or purchase in the country from which you are accessing this site and/or will have any products delivered to.  </li>
+                                            </ul>
+                                            <p>
+                                                We do not guarantee that the ST site(s) will be secure or free from bugs or viruses. We are not responsible for such viruses and you must not introduce them. 
+                                            </p>
+                                            <ul>
+                                                <li>Viruses, trojans, worms, logic bombs or other material that is malicious or technologically harmful  </li>
+                                                <li>Misu LST Enforcement </li>
+                                                <li>Attempt to gain unauthorized access to the ST site(s), the server on which the ST site(s) is stored, or any server, computer, or database connected to the ST site(s)  </li>
+                                                <li>Attack the ST site(s) via a denial-of-service attack or a distributed denial-of service attack. </li>
+                                            </ul>
+                                            
+                                            <h3>Other ST Terms, Policies and Agreements </h3>
+                                            <p>
+                                                Other Terms, Policies and Agreements may apply to the supply of any products or services by the ST site(s) guest and/or registered Users. 
+                                            </p>
+
+                                            <p>
+                                                Last Updated: Friday 30 October 2020 
+                                            </p>
+                                            
+
+                                        </div>
+
+
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
+}
+export default MarketplaceAgreement;

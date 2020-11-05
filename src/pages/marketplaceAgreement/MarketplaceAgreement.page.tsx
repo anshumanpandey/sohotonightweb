de87026ce60f5df3d22d939b567d9cebdf9cfa10
leo <leo_9_t@hotmail.com>
2020-11-05 13:15:33 -0400

    COMPLETED page

--- src/pages/marketplaceAgreement/MarketplaceAgreement.page.tsx
+++ src/pages/marketplaceAgreement/MarketplaceAgreement.page.tsx
@@ -149,16 +149,121 @@ function MarketplaceAgreement() {
                                                 <li>Webcam or Phone Chat sessions </li>
                                                 <li>Any other items for which is it illegal to sell or purchase in the country from which you are accessing this site and/or will have any products delivered to.  </li>
                                             </ul>
-                                            <p>
-                                                We do not guarantee that the ST site(s) will be secure or free from bugs or viruses. We are not responsible for such viruses and you must not introduce them. 
-                                            </p>
-                                            <ul>
-                                                <li>Viruses, trojans, worms, logic bombs or other material that is malicious or technologically harmful  </li>
-                                                <li>Misu LST Enforcement </li>
-                                                <li>Attempt to gain unauthorized access to the ST site(s), the server on which the ST site(s) is stored, or any server, computer, or database connected to the ST site(s)  </li>
-                                                <li>Attack the ST site(s) via a denial-of-service attack or a distributed denial-of service attack. </li>
-                                            </ul>
+
+                                            <p style={{ fontWeight: 'bold' }}>Important Notes </p>
+                                            <p>Even if you offer to give away for "free" (rather than sell) a prohibited item, this will not relieve you of potential liability. This applies to both Seller and Buyer.  </p>
+                                            <p>This list is updated from time to time and is incorporated by reference into the various website use agreements. These guidelines do not constitute legal advice and do not pertain to any particular company's practices. When in doubt, check with law enforcement agencies, a lawyer, the law or with a copyright, trademark or other rights owner for clarification. </p>
+                                            <p>Please note that the above list is neither exhaustive nor restrictive. Sohotonight.com reserves the right to delete any listing that may breach any legal provision or the general principles and values of the Sohotonight.com community, even if the legal provision or principles and values are not explicitly stated on the site. </p>
                                             
+                                            <h3>Terms and Conditions of the Official Stores  </h3>
+                                            <p>The Official Stores are bound by the same Prohibited Listings with exception to the provision of condoms, which will only be sold as new. </p>
+                                            <p>Delivery & Returns Information for the Sex Toys Official Store </p>
+                                            <p style={{ fontWeight: 'bold' }}>Delivery  </p>
+                                            <p>We will endeavor to dispatch all orders that are placed before 4pm on the same day, Monday to Friday. Orders that are placed on a Friday after the cut-off time will be shipped on the following Monday. </p>
+                                            <p>There are three options for UK delivery: </p>
+                                            <ul>
+                                                <li>£2.95 for Royal Mail 48 Tracked Delivery usually taking 2-5 working days </li>
+                                                <li>£4.95 for Royal Mail Tracked 24 Delivery, delivered next working day* </li>
+                                                <li>£5.95 for Interlink Next Day Tracked Delivery, delivered the next working day* </li>
+                                            </ul>
+                                            <p>* 2 working days for certain AB, DD, FK, TD, PH postcodes, Northern Ireland, the Highlands and Islands of Scotland, and the Isles of Scilly </p>
+                                            <p>European orders are delivered for £10 GBP by Royal Mail within 3-10 working days. Delivery to the rest of the world costs £19.95 GBP and usually takes between 7-10 working days. </p>
+                                            <p>All delivery prices are exclusive of VAT which will be applied at checkout. Please note that items delivered outside the UK carry a weight limit (2kg for Europe, 1.5kg for outside Europe), items over these limits may incur extra delivery charges and customs fees. We will contact you regarding any extra charges where applicable. </p>
+
+                                            <p style={{ fontWeight: 'bold' }}>Missed Deliveries </p>
+
+                                            <p style={{ fontWeight: 'bold' }}>Royal Mail  </p>
+                                            <p>Royal Mail will hold your order at one of their Customer Service Points for 18 days if they have been unable to deliver to you or you do not collect your order. After this time the item(s) will be returned to the shop and we will contact you regarding how you wish to proceed. </p>
+                                            <p>Orders sent outside the UK (Europe & rest of the world) </p>
+                                            <p>If the order was originally sent to Europe or elsewhere, the full shipping cost will need to be paid again so £10 GBP (+VAT) for the EU and £19.95 GBP (+VAT) for International orders. </p>
+                                            
+                                            <p style={{ fontWeight: 'bold' }}>Returns </p>
+                                            <p>Freepost return labels will be provided on your behalf for any returns, faulty or unwanted. </p>
+
+                                            <p style={{ fontWeight: 'bold' }}>Returns </p>
+                                            <p>Freepost return labels will be provided on your behalf for any returns, faulty or unwanted. </p>
+
+
+                                            <p style={{ fontWeight: 'bold' }}>Unwanted goods  </p>
+                                            <p>Any credit for goods returned to us as unwanted will be less a 25% restocking charge, subject to the following conditions: </p>
+                                            <ul>
+                                                <li>The goods must be unused </li>
+                                                <li>The goods must be in their original condition & packaging (including shrink wrapping, seals etc) </li>
+                                                <li>The goods must be received by us within 14 days from the day you receive your order (missed deliveries are exempt from this condition)  </li>
+                                            </ul>
+                                            <p>If we suspect that the goods have been used or any of the above conditions are not met then we reserve the right to return the goods to you. Items supplied in sealed packaging that have been opened are non-refundable and non-exchangeable. </p>
+
+                                            <p style={{ fontWeight: 'bold' }}>Faulty goods </p>
+                                            <p>We operate a one year returns policy for faulty goods. Please send the item back to us with a note stating the problem, the desired outcome (exchange or refund) and the order reference number for the original purchase; which we will advise you of. We will exchange the item for you or credit your account if this is not possible. Please get in touch with us here for assistance. </p>
+                                            <p>You will not be charged any additional costs for the exchange of damaged or faulty goods. </p>
+
+                                            <p style={{ fontWeight: 'bold' }}>Damaged goods & incomplete orders </p>
+                                            <p>All claims for delivery shortages and damages must be reported to us within 48 hours of the delivery date. Any claims of this nature that are outside of this time frame will not be honored. Please get in touch with us here for assistance. </p>
+                                            <p>You will not be charged any additional costs for the exchange of damaged or faulty goods. </p>
+                                            <p>Delivery & Returns Information for the Lingerie Shop Official Store </p>
+
+                                            <p style={{ fontWeight: 'bold' }}>Delivery </p>
+                                            <p>Orders placed before 14:00 Monday to Friday will normally be dispatched the same day. Orders placed after these times will be dispatched on the next working day (Monday to Friday). All First-Class UK orders should arrive next day or the day after, depending on where you live and the time of day you order. Orders shipped via First Class Recorded are not a guaranteed next day service. </p>
+                                            <ul>
+                                                <li>'UK Special Delivery 1pm Signed For' items are not delivered at the weekends, only Mondays to Fridays. </li>
+                                                <li>'UK Special Delivery 1pm Signed For (Saturdays)' items are delivered on Saturdays. </li>
+                                                <li>All goods are wrapped discreetly. It will not be possible for anyone to identify the ordered goods, or their source from the outside. </li>
+                                                <li>All goods are dispatched from the UK using Royal Mail.</li>
+                                                <li>Recorded Deliveries will require a signature upon delivery </li>
+                                            </ul>
+                                            <p>Wishlist orders will not be dispatched for 24 hours unless the Wishlist Delivery Address is updated/confirmed in the meantime. If that delivery address is updated/confirmed before 14:00 Monday to Friday the Lingerie Shop will normally dispatch the same day. </p>
+
+                                            <p style={{ fontWeight: 'bold' }}>Returns </p>
+                                            <p>Returns will only be accepted with a valid Returns Authorization Number, which can be obtained here. </p>
+                                            <ul>
+                                                <li>Return Numbers must be obtained within two weeks of delivery. </li>
+                                                <li>Faulty items will be replaced at no cost to yourself. </li>
+                                                <li>Exchanges for items of the same value (different size/color) are permitted but you will need to pay the delivery charges. </li>
+                                                <li>Incorrectly shipped items or orders will be corrected at our expense. </li>
+                                                <li>Refunds will only be issued for non-faulty products if they have not been opened and are in a saleable condition.  </li>
+                                            </ul>
+                                            <p>If your goods have not arrived after one week, report this to us here. If your order has arrived but items are missing, report this to us here. </p>
+                                            <p>If you have any other problems or would like to get in touch, please use the Help Centre. </p>
+
+                                            <h3>Liability Limitation </h3>
+                                            <p>ST and services are offered "as is" and "as available", without any warranties or guarantees, express or implied. We and our suppliers specifically disclaim the implied warranties of title, merchantability, fitness for a particular purpose and non-infringement. </p>
+                                            <p>This does not apply to death or personal injury caused by our negligence and does not apply to any fraudulent misrepresentation by us. </p>
+                                            <p>ST is not and cannot be responsible for the behavior of, or the items offered for sale or supply by, the Seller. </p>
+                                            <p>ST has no control over, and does not guarantee the existence of quality, safety or legality of goods advertised, accuracy of Users’ content, listings or information about the goods being offered. Even though Members are prohibited from doing so, some may provide information, or otherwise behave, in a way that is unreliable, illegal, or in breach of a Members' obligations under the Site’s Policies & Agreements. </p>
+                                            <p>ST cannot and does not control the completion or validity of transactions; </p>
+                                            <ul>
+                                                <li>ST does not check the ability of Buyers to pay for items or Sellers availability of stock. As such, ST does not guarantee the transactions will take place. Users are alerted to exercise no lesser degree of caution in entering into transactions, whether as a Seller or Buyer, than you would exercise when entering into a similar transaction offline. </li>
+                                            </ul>
+
+                                            <p style={{ fontWeight: 'bold' }}>Disclaimer </p>
+                                            <p>ST does not and cannot have possession of anything listed or sold through the Marketplace or Official Stores. Marketplace Sellers and Buyers are urged to offer and use one of the Royal Mail recorded delivery services respectively. </p>
+
+                                            <p style={{ fontWeight: 'bold' }}>Indemnification </p>
+                                            <p>The Buyer agrees that they will be liable for any losses howsoever incurred from accessing and/or using the ST site(s). </p>
+                                            <p>In no event shall ST (including our suppliers, officers, directors, employees) be liable for lost profits, revenues, or any special, incidental or consequential damages (however arising, including negligence) resulting from, or in connection with this Agreement or the use of the ST site(s). </p>
+                                            <p>You agree to indemnify and hold harmless ST, its officers, employees, parent companies, subsidiaries, contractors, vendors or affiliates, from any claim or demand, including reasonable solicitors' fees, made by any third party due to or arising out of your use of the ST Auctions and Sales, the violation of the Site's Policies & Agreements by you, or the infringement by you or any other User of ST using your computer/account, of any intellectual property or other right of any person or entity. </p>
+                                            <p>ST, which is to include our parent, subsidiaries, employees, affiliates and directors, will not be liable to you in contract, tort, negligence or otherwise for any business losses, such as loss of revenue, profits, data, opportunities, goodwill, reputation or interruption of business or any other losses arising from: </p>
+                                            <ul>
+                                                <li>Viruses or other malicious software acquired by accessing our marketplace or other area of the ST site(s)</li>
+                                                <li>Damage to your hardware device as a result of using ST </li>
+                                                <li>Your usage of ST or your inability to use ST </li>
+                                                <li>Delays or disruptions in our services and interruption of access of ST; including as a result of errors, problems with connectivity or any inaccuracies of any kind in our services</li>
+                                                <li>Pricing, postage or anything else related to your listing</li>
+                                            </ul>
+
+                                            <p>In the case where we are found liable, our liability to you is limited to the price of the item in the listing. </p>
+
+                                            <p style={{ fontWeight: 'bold' }}>International Transactions</p>
+                                            <p>You, as a Buyer or Seller are responsible for complying with all laws and regulations applicable to international sales, purchases and postage of goods. </p>
+
+                                            <p style={{ fontWeight: 'bold' }}>Validity of Provisions </p>
+                                            <p>If any clause or part thereof is held to be invalid, void or is unenforceable for any reason, it shall be struck out and shall not affect the validity and enforceability of the remainder of the Marketplace Terms & Conditions. </p>
+
+
+                                            <p style={{ fontWeight: 'bold' }}>Changes to this Agreement  </p>
+                                            <p>We may change this Agreement from time to time, either to add or remove specific types of Auction or Sale, or to alter the way that individual Auctions are conducted. Therefore, you should keep up to date with their content. If you have not yet participated in a type of Auction or Sale you find on ST, then we encourage you to read this Agreement before you take part. Indeed, even if you regularly buy or sell by Auction/Sale, you should take the time to read them. By taking part in an Auction or Sale, you indicate that you have done so and are obliged to comply with them. </p>
+
+
                                             <h3>Other ST Terms, Policies and Agreements </h3>
                                             <p>
                                                 Other Terms, Policies and Agreements may apply to the supply of any products or services by the ST site(s) guest and/or registered Users. 

ce6cd4ccbc697e6cd793c077ea3618b2e092b25c
leo <leo_9_t@hotmail.com>
2020-11-23 13:35:43 -0400

    FIx on style

--- src/pages/ListPosts/ListPostItem.tsx
+++ src/pages/ListPosts/ListPostItem.tsx
@@ -7,7 +7,7 @@ const ListPostItem = ({ girl: g } : { girl: any }) => {
     const isMobile = UseIsMobile();
     return (
         <div style={{ display: 'flex', flexDirection: 'row', borderBottom: "1px solid #d8d8d8" }}>
-            <div style={{ paddingLeft: 0, paddingTop: '2rem', paddingBottom: '2rem', paddingRight: '2rem' }}>
+            <div style={{ flex: 1,paddingLeft: 0, paddingTop: '2rem', paddingBottom: '2rem', paddingRight: '2rem' }}>
                 <Link style={{ display: "flex", justifyContent: "center" }} to={`/profile/${g.id}`}>
                     <img style={{ borderRadius: "50%", maxWidth: "100%", maxHeight: 100 }} src={g.profilePic || "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"} className="img-responsive" alt="profile" />
                 </Link>

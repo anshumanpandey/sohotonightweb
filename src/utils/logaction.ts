569c64509ed5d01818a316b7d44045a2f886b7ce
Anshuman <Anshuman>
2021-06-11 16:03:22 -0400

    ADDED more log

--- src/utils/logaction.ts
+++ src/utils/logaction.ts
@@ -1,5 +1,6 @@
 import { AxiosInstance } from "./AxiosBootstrap"
 
 export const logActionToServer = ({ body }: { body: string }) => {
+    console.log(body)
     return AxiosInstance({ url: '/log', method: 'post', data: { body }})
 }

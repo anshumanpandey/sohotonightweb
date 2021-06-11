febfbb65c17dd8ed1f8e7e6b7d1d236a79ee61de
Anshuman <Anshuman>
2021-06-11 16:39:00 -0400

    FIR order

--- src/utils/PeerClient.ts
+++ src/utils/PeerClient.ts
@@ -4,10 +4,10 @@ export const buildPeerClient = (p?: SimplePeer.Options) => {
     return new SimplePeer({
         config: {
             iceServers: [
-                { urls: 'stun:stun.sohotonight.com:5349?transport=tcp' },
                 { urls: 'stun:stun.l.google.com:19302' },
                 { urls: 'stun:global.stun.twilio.com:3478?transport=udp' },
                 { urls: 'stun:stun.services.mozilla.com'},
+                { urls: 'stun:stun.sohotonight.com:5349?transport=tcp' },
             ]
         },
         ...p

14178f3a312b0982cb0f8b4f0f68c42e473d7b3b
Anshuman <Anshuman>
2021-06-11 16:27:18 -0400

    ADDED extra webtrc servers

--- src/utils/PeerClient.ts
+++ src/utils/PeerClient.ts
@@ -2,7 +2,14 @@ import SimplePeer from 'simple-peer';
 
 export const buildPeerClient = (p?: SimplePeer.Options) => {
     return new SimplePeer({
-        config: { iceServers: [{ urls: 'stun:stun.sohotonight.com:5349?transport=tcp' }] },
+        config: {
+            iceServers: [
+                { urls: 'stun:stun.sohotonight.com:5349?transport=tcp' },
+                { urls: 'stun:stun.l.google.com:19302' },
+                { urls: 'stun:global.stun.twilio.com:3478?transport=udp' },
+                { urls: 'stun:stun.services.mozilla.com'},
+            ]
+        },
         ...p
     })
 }

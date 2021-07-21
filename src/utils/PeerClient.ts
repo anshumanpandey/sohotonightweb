b0b48bcd195a1ed49250151578a5849cd3a01415
Anshuman <Anshuman>
2021-07-21 08:20:28 -0400

    ADDED param

--- src/utils/PeerClient.ts
+++ src/utils/PeerClient.ts
@@ -5,6 +5,10 @@ export const buildPeerClient = (p?: SimplePeer.Options) => {
         config: {
             iceServers: [
                 { url: 'stun:stun.sohotonight.com:5349', urls: 'stun:stun.sohotonight.com:5349' },
+                {
+                    "url": "turn:turn.sohotonight.com:5349",
+                    "urls": "turn:turn.sohotonight.com:5349",
+                }
                 //{'url': 'stun:stun.l.google.com:19302', 'urls': 'stun:stun.l.google.com:19302'}
             ]
         },

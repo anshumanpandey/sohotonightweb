5e4982b32131fb6bd328f176d654809b0003f87a
Anshuman <Anshuman>
2021-07-21 07:35:40 -0400

    TEST another ICE server

--- src/utils/PeerClient.ts
+++ src/utils/PeerClient.ts
@@ -4,7 +4,8 @@ export const buildPeerClient = (p?: SimplePeer.Options) => {
     return new SimplePeer({
         config: {
             iceServers: [
-                { urls: 'stun:stun.sohotonight.com:5349?transport=tcp' },
+                //{ urls: 'stun:stun.sohotonight.com:5349?transport=tcp' },
+                {'url': 'stun:stun.l.google.com:19302', 'urls': 'stun:stun.l.google.com:19302'}
             ]
         },
         ...p

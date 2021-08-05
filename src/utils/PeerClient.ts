145c167cd4d80ea6649879184daeb6d071380030
Anshuman <Anshuman>
2021-08-05 15:43:05 -0400

    INCREASE library version; ADDED debug parameter

--- src/utils/PeerClient.ts
+++ src/utils/PeerClient.ts
@@ -3,14 +3,13 @@ import SimplePeer from 'simple-peer';
 export const buildPeerClient = (p?: SimplePeer.Options) => {
     return new SimplePeer({
         config: {
+            debugger: true,
             iceServers: [
                 {
-                    url: 'stun:stun.sohotonight.com:5349',
                     urls: 'stun:stun.sohotonight.com:5349',
                     "username": "admin",
                     "credential": "123456abc!" },
                 {
-                    "url": "turn:turn.sohotonight.com:5349",
                     "urls": "turn:turn.sohotonight.com:5349",
                     "username": "admin",
                     "credential": "123456abc!"
